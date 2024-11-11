2168b29ad2af67ea7ce5fd79d43a053f890ec46c
kathryntanardy <ktanardy@gmail.com>
2024-11-10 16:43:09 -0800

    Added HomePage Components

--- components/Bluetooth.js
+++ components/Bluetooth.js
@@ -1,16 +1,14 @@
 import React, { useState } from 'react';
 import { View, TextInput, Text, TouchableOpacity, StyleSheet } from 'react-native';
-import Bluetooth from './Bluetooth'
 
-const Home = () => {
+const Bluetooth = () => {
 
   return (
     <View style={styles.container}>
     <Text>Home Page</Text>
-    <Bluetooth/>
     </View>
   );
 };
 
 
-export default Home;
+export default Bluetooth;
