2c6210abf1390d80329d1b02c64b35596da33e1c
kathryntanardy <ktanardy@gmail.com>
2024-11-19 22:40:15 -0800

    Added NavBar

--- components/Home.js
+++ components/Home.js
@@ -1,14 +1,20 @@
 import React, { useState } from 'react';
 import { View, TextInput, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import Bluetooth from './Bluetooth'
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+
+
+const Tab = createMaterialTopTabNavigator();
 
 const Home = () => {
 
+
   return (
-    <View style={styles.container}>
-    <Text>Home Page</Text>
-    <Bluetooth/>
+    <View>
+      <Text>Placeholder Home</Text>
     </View>
+
   );
 };
 
